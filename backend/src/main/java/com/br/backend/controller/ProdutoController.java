b223ca5bdb47b15f83e40772f1280cafa15116eb
unknown <jeanrojas2007@outlook.com>
2021-12-14 17:17:42 -0300

    controller

--- backend/src/main/java/com/br/backend/controller/ProdutoController.java
+++ backend/src/main/java/com/br/backend/controller/ProdutoController.java
@@ -1,4 +1,45 @@
 package com.br.backend.controller;
 
+import com.br.backend.dto.ProdutoDTO;
+import com.br.backend.dto.form.ProdutoForm;
+import com.br.backend.service.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/produto")
 public class ProdutoController {
+
+    @Autowired
+    private ProdutoService produtoService;
+
+    public ProdutoController(ProdutoService produtoService) {
+        this.produtoService = produtoService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProdutoDTO>> findAll(){
+        return ResponseEntity.ok(produtoService.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<ProdutoDTO> create(@RequestBody @Valid ProdutoForm produtoForm){
+        return ResponseEntity.ok(ProdutoService.create(produtoForm));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProdutoDTO> update(@PathVariable Long id, @RequestBody @Valid ProdutoForm produtoForm){
+        return ResponseEntity.ok(produtoService.update(id, produtoForm));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id){
+        produtoService.delete(id);
+        return ResponseEntity.ok().build();
+    }
+    
 }
